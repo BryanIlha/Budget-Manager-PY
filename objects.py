036a6346685922fb24520719f9447736a7e22632
Bruno Muller <bruno_muller0706@outlook.com>
2024-02-16 00:29:21 -0300

    arruma ai pau no uc

--- objects.py
+++ objects.py
@@ -1,4 +1,6 @@
-from tkinter import ttk 
+from tkinter import ttk
+import customtkinter as ctk
+
 class Table():
     def __init__(self, parent):
         columns = ('Unidade', 'Quantidade', 'Nome', 'Valor Unitario', 'Valor Total')
@@ -18,4 +20,22 @@ class Item: #objeto referente aos itens do orçamento
         self.valor_total = quantidade * valor_uni
 
 
+class TopLevelWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        new_window.title("New Window")
+
+        labels = ["Unidade", "Quantidade", "Nome", "Valor Unitário", "Valor Total"]
+        entries = []
+
+        for i, label_text in enumerate(labels):
+            label = ctk.CTkLabel(new_window, text=label_text + ":",)
+            label.grid(row=i, column=0, padx=5, pady=5, sticky="w")
+
+            entry = ctk.CTkEntry(new_window)
+            entry.grid(row=i, column=1, padx=5, pady=5, sticky="ew")
+            entries.append(entry)
+
+        add_button = ctk.CTkButton(new_window, text="Adicionar" )
+        add_button.grid(row=len(labels)+1, columnspan=2, pady=10)
 
