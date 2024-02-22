2a9b223bc6140a720dd6f54d40607388e4e94992
Bryan Salgueiro <bryansalgueiro@gmail.com>
2024-02-21 21:23:52 -0300

    last att

--- front.py
+++ front.py
@@ -50,21 +50,39 @@ class MainWindow(ctk.CTk):
 
     def create_main_section(self):
         self.main_section = ctk.CTkFrame(self.master,)
-        self.main_section.pack(expand=True, fill="both", padx=20, pady=20)
+        self.main_section.pack(expand=True, fill="both", padx=20, pady=10)
 
         title_label = ctk.CTkLabel(self.main_section, text="Gerenciador de orçamentos", font=("Arial", 20))
         title_label.pack(pady=10)
+
+        self.option_table = ctk.CTkOptionMenu(self.main_section ,values=["NAO FUNCIONA","NAO FUNCIONA"])
+        self.option_table.configure(width=300)
+        self.option_table.pack(anchor="center")
+
 
         table_frame = ctk.CTkFrame(self.main_section)
         table_frame.pack(expand=True, fill="both", padx=10, pady=10)
         self.table = Table(table_frame)
 
         button_frame = ctk.CTkFrame(self.master,)
-        button_frame.pack(side="bottom", anchor="se", padx=20, pady=20)
+        button_frame.pack(fill="both",  padx=20, pady=10) #devo adicionar side?
 
+        self.bt_table = ctk.CTkButton(button_frame, text="Novo Serviço")
+        self.bt_table.configure(fg_color="purple",
+                                hover_color="blue",
+                                width=80,
+                                height=80)
+        self.bt_table.pack(side="left", pady=5)
 
-        self.button = ctk.CTkButton(button_frame, text="Novo item",command=self.open_topLevel)
-        self.button.pack(pady=5)
+        self.bt_item = ctk.CTkButton(button_frame, text="Novo item",command=self.open_topLevel)
+        self.bt_item.configure(fg_color="green",
+                                width=80,
+                                height=80)
+        self.bt_item.pack(side="right",pady=5)
+
+        # self.option_table = ctk.CTkOptionMenu(button_frame, values=["Table1","Table2"])
+        # self.option_table.pack(side="bottom")
+
     def open_topLevel(self):
             if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
                 self.toplevel_window = TopLevelWindow(self,self.table)
