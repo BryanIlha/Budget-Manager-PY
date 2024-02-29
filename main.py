e26a22064089a4e4fc3a12de8e674785b401d36d
Bruno Muller <bruno_muller0706@outlook.com>
2024-02-29 04:53:27 -0300

    adicionando modais de confirmação

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 # from funcion import * # importando todas funções
-from objects import Table, TopLevelWindow,Item,LoadWindow #objetos como tabela e items
-from funcion import create_excel, new_service, delete_service , clear_table, change_service, save_dict, load_dict
+# objetos como tabela e items
+from objects import Table, TopLevelWindow, Item, LoadWindow
+from funcion import create_excel, new_service, delete_service, clear_table, change_service, save_dict, load_dict
 
 
 ctk.set_appearance_mode("dark")
 
 
-
 ctk.set_default_color_theme("green")
+
+# root = tk.Tk()
+
 
 class MainWindow(ctk.CTk):
     def __init__(self,  *args, **kwargs):
+        # global root
+        # self.root = root
         super().__init__(*args, **kwargs)
 
-        self.simple_counter = 0 #revisar first time counter
-        self.dict_serv={} #lista de serviços utizilar em condicionais para nao deixar exportar sem nenhum serviço
-        
+        self.simple_counter = 0  # revisar first time counter
+        # lista de serviços utizilar em condicionais para nao deixar exportar sem nenhum serviço
+        self.dict_serv = {}
+
         # self.self.button_frame= None
-        self.toplevel_window = None #burocracia pra ter toplevel window
-        
+        self.toplevel_window = None  # burocracia pra ter toplevel window
 
         self.title("Custom Tkinter App")
         self.geometry("800x600")
@@ -30,26 +35,24 @@ class MainWindow(ctk.CTk):
 
         # Configure dark blue theme
 
-        
         self.create_sidebar()
         self.first_service()
-        
-        
+
     def first_service(self):
         self.main_section = ctk.CTkFrame(self.master,)
         self.main_section.pack(expand=True, fill="both", padx=20, pady=10)
 
-        title_label = ctk.CTkLabel(self.main_section, text="Gerenciador de orçamentos", font=("Arial", 20))
+        title_label = ctk.CTkLabel(
+            self.main_section, text="Gerenciador de orçamentos", font=("Arial", 20))
         title_label.pack(pady=10)
 
-        self.bt_serv = ctk.CTkButton(self.main_section, text="Novo Serviço", command= lambda:self.first_serv())
+        self.bt_serv = ctk.CTkButton(
+            self.main_section, text="Novo Serviço", command=lambda: self.first_serv())
         self.bt_serv.configure(fg_color="purple",
-                                hover_color="blue",
-                                width=80,
-                                height=80)
+                               hover_color="blue",
+                               width=80,
+                               height=80)
         self.bt_serv.pack(side="top", pady=5)
-               
-    
 
     def create_sidebar(self):
         self.sidebar = ctk.CTkFrame(self.master, width=200)
@@ -58,106 +61,103 @@ class MainWindow(ctk.CTk):
         profile_photo = ctk.CTkLabel(self.sidebar, text="Profile Photo",)
         profile_photo.pack(pady=20)
 
-        section1_button = ctk.CTkButton(self.sidebar, text="Section 1",command= lambda:create_excel()) #nao funciona
+        section1_button = ctk.CTkButton(
+            self.sidebar, text="Section 1", command=lambda: create_excel())  # nao funciona
         section1_button.pack(pady=5, padx=10, fill="x")
 
-        save_btn = ctk.CTkButton(self.sidebar, text="Salvar", command= lambda:save_dict(self)  ) #tirar daqui depois 
+        save_btn = ctk.CTkButton(self.sidebar, text="Salvar", command=lambda: save_dict(
+            self))  # tirar daqui depois
         save_btn.pack(pady=5, padx=10, fill="x")
 
-        load_btn = ctk.CTkButton(self.sidebar, text="Carregar", command= lambda:load_dict(self,Item,LoadWindow)  ) #tirar daqui depois 
+        load_btn = ctk.CTkButton(self.sidebar, text="Carregar", command=lambda: load_dict(
+            self, Item, LoadWindow))  # tirar daqui depois
         load_btn.pack(pady=5, padx=10, fill="x")
         self.button_frame = ctk.CTkFrame(self.master,)
 
     def first_serv(self):
-        
 
-        if new_service(self) is True or self.dict_serv !={}:
-           self.create_main_section()
-            
+        if new_service(self) is True or self.dict_serv != {}:
+            self.create_main_section()
+
     def create_main_section(self):
-            self.bt_serv.destroy()
-            dict_serv_keys = list(self.dict_serv.keys())
+        self.bt_serv.destroy()
+        dict_serv_keys = list(self.dict_serv.keys())
 
-            self.option_serv = ctk.CTkOptionMenu(self.main_section ,
-                                                values=dict_serv_keys,
-                                                command=self.switch_service)
-            self.option_serv.configure(width=300)
-            self.option_serv.pack(anchor="center")
+        self.option_serv = ctk.CTkOptionMenu(self.main_section,
+                                             values=dict_serv_keys,
+                                             command=self.switch_service)
+        self.option_serv.configure(width=300)
+        self.option_serv.pack(anchor="center")
 
+        table_frame = ctk.CTkFrame(self.main_section)
+        table_frame.pack(expand=True, fill="both", padx=10, pady=10)
+        self.table_instance = Table(table_frame)
 
-            table_frame = ctk.CTkFrame(self.main_section)
-            table_frame.pack(expand=True, fill="both", padx=10, pady=10)
-            self.table_instance = Table(table_frame)
+        self.button_frame = ctk.CTkFrame(self.master,)
+        self.button_frame.pack(fill="both",  padx=20,
+                               pady=10)  # devo adicionar side?
 
-            self.button_frame = ctk.CTkFrame(self.master,)
-            self.button_frame.pack(fill="both",  padx=20, pady=10) #devo adicionar side?
+        self.bt_serv = ctk.CTkButton(
+            self.button_frame, text="Novo Serviço", command=lambda: new_service(self))
+        self.bt_serv.configure(fg_color="purple",
+                               hover_color="blue",
+                               width=80,
+                               height=80)
+        self.bt_serv.pack(side="left", pady=5)
 
-            self.bt_serv = ctk.CTkButton(self.button_frame, text="Novo Serviço",command=lambda: new_service(self))
-            self.bt_serv.configure(fg_color="purple",
-                                    hover_color="blue",
-                                    width=80,
-                                    height=80)
-            self.bt_serv.pack(side="left", pady=5)
+        self.bt_del_serv = ctk.CTkButton(
+            self.button_frame, text="Deletar Serviço", command=lambda: delete_service(self))
+        self.bt_del_serv.configure(fg_color="red",
+                                   hover_color="black",
+                                   width=80,
+                                   height=80)
+        self.bt_del_serv.pack(side="left", pady=5)
 
-            self.bt_del_serv = ctk.CTkButton(self.button_frame, text="Deletar Serviço",command=lambda:delete_service(self))
-            self.bt_del_serv.configure(fg_color="red",
-                                    hover_color="black",
-                                    width=80,
-                                    height=80)
-            self.bt_del_serv.pack(side="left", pady=5)
+        self.bt_item = ctk.CTkButton(
+            self.button_frame, text="Novo item", command=self.open_topLevel)
+        self.bt_item.configure(fg_color="green",
+                               width=80,
+                               height=80)
+        self.bt_item.pack(side="right", pady=5)
 
-            self.bt_item = ctk.CTkButton(self.button_frame, text="Novo item",command=self.open_topLevel)
-            self.bt_item.configure(fg_color="green",
-                                    width=80,
-                                    height=80)
-            self.bt_item.pack(side="right",pady=5)
+    def open_topLevel(self):  # new item
+        service_name = self.option_serv.get()
 
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = TopLevelWindow(
+                self, self.table_instance, service_name, self.dict_serv)
+        else:
+            self.toplevel_window.focus()
 
-    def open_topLevel(self): #new item
-            service_name=self.option_serv.get()
-            
-            if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-                self.toplevel_window = TopLevelWindow(self,self.table_instance,service_name,self.dict_serv)
-            else:
-                self.toplevel_window.focus()
-        
-    
-
-    def switch_service(self,choice): #precisa?
+    def switch_service(self, choice):  # precisa?
         change_service(self)
-        
 
-
-    
-    def update_service(self,current_index,dict_keys):
-    # Obtém as chaves atualizadas do dicionário
-        
+    def update_service(self, current_index, dict_keys):
+        # Obtém as chaves atualizadas do dicionário
 
         # Limpa o valor selecionado no OptionMenu
         self.option_serv.set('')
         # Verifica se há chaves no dicionário
         # Encontra a posição da chave atualmente selecionada no OptionMenu
-        
 
-            
         # Calcula o índice da próxima chave no OptionMenu
         next_index = (current_index + 1) % len(dict_keys)
- 
-            
+
         # Seleciona a próxima chave no OptionMenu
         self.option_serv.set(dict_keys[next_index])
         self.option_serv.configure(values=self.dict_serv)
         change_service(self)
 
-    def clean_frame(self,frame):
-         for widget in frame.winfo_children():
-              widget.destroy()
-         
+    def clean_frame(self, frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
+
 
 def main():
 
     app = MainWindow()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- objects.py
+++ objects.py
@@ -1,46 +1,48 @@
 from tkinter import ttk
 import customtkinter as ctk
-from funcion import new_item, add_to_table , obter_nomes_saves
+from funcion import new_item, add_to_table, obter_nomes_saves
 
 
 class Table():
     def __init__(self, parent):
-        columns = ('Nome', 'Unidade', 'Quantidade', 'Valor Unitario', 'Valor Total')
+        columns = ('Nome', 'Unidade', 'Quantidade',
+                   'Valor Unitario', 'Valor Total')
         self.treeview = ttk.Treeview(parent, columns=columns, show='headings')
         self.treeview.pack(expand=True, fill="both")
         style = ttk.Style()
         style.theme_use("default")
-    
+
         style.configure("Treeview",
-                            background="#2a2d2e",
-                            foreground="white",
-                            rowheight=20,
-                            fieldbackground="#343638",
-                            bordercolor="#343638",
-                            borderwidth=0)
+                        background="#2a2d2e",
+                        foreground="white",
+                        rowheight=20,
+                        fieldbackground="#343638",
+                        bordercolor="#343638",
+                        borderwidth=0)
         style.map('Treeview', background=[('selected', '#22559b')])
-    
+
         style.configure("Treeview.Heading",
-                            background="#565b5e",
-                            foreground="white",
-                            relief="flat",
-                            font=("Arial", 12))
+                        background="#565b5e",
+                        foreground="white",
+                        relief="flat",
+                        font=("Arial", 12))
         style.map("Treeview.Heading",
-                      background=[('active', '#3484F0')])
+                  background=[('active', '#3484F0')])
         for col in columns:
             self.treeview.heading(col, text=col)
             self.treeview.column(col, width=20)
 
-class Item: #objeto referente aos itens do orçamento
+
+class Item:  # objeto referente aos itens do orçamento
     def __init__(self, nome, unidade, quantidade,
-                  valor_uni, valor_total):
+                 valor_uni, valor_total):
         self.nome = nome
         self.unidade = unidade
         self.quantidade = quantidade
         self.valor_uni = valor_uni
         self.valor_total = valor_total
 
-    def to_dict(self): #iterar o objeto para jogar no json depois
+    def to_dict(self):  # iterar o objeto para jogar no json depois
         return {
             'nome': self.nome,
             'unidade': self.unidade,
@@ -50,18 +52,30 @@ class Item: #objeto referente aos itens do orçamento
         }
 
 
+class TopLevelConfirmModal(ctk.CTkToplevel):
+    def __init__(self, master, title, text, button_texts):
+        super().__init__(master)
+        self.title(title)
+        self.label = ctk.CTkLabel(self, text=text)
+        self.label.pack(padx=10, pady=10)
+        self.buttons = []
+        for button_text in button_texts:
+            button = ctk.CTkButton(self, text=button_text)
+            button.pack(padx=10, pady=10)
+            self.buttons.append(button)
+
 
 class TopLevelWindow(ctk.CTkToplevel):
-    def __init__(self, master,table_instance,service_name,dict_serv,*args, **kwargs):
-        super().__init__(master,*args, **kwargs)
+    def __init__(self, master, table_instance, service_name, dict_serv, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
         self.title("New Window")
         self.resizable(False, False)
-        self.dict_serv= dict_serv
-        self.service_name= service_name
+        self.dict_serv = dict_serv
+        self.service_name = service_name
         self.table_instance = table_instance
 
-
-        labels = ["Nome", "Unidade", "Quantidade", "Valor Unitário", "Valor Total"]
+        labels = ["Nome", "Unidade", "Quantidade",
+                  "Valor Unitário", "Valor Total"]
         self.entries = []
 
         for i, label_text in enumerate(labels):
@@ -73,44 +87,39 @@ class TopLevelWindow(ctk.CTkToplevel):
             self.entries.append(entry)
 
         self.switch_var = ctk.StringVar(value="off")
-        switch = ctk.CTkSwitch(self, text="Calcular Valor Total", variable=self.switch_var, command=self.switch_event,onvalue="on", offvalue="off")
+        switch = ctk.CTkSwitch(self, text="Calcular Valor Total", variable=self.switch_var,
+                               command=self.switch_event, onvalue="on", offvalue="off")
         switch.grid(row=len(labels), columnspan=2, pady=10, padx=5, sticky="e")
 
-        add_button = ctk.CTkButton(self, text="Adicionar",command=lambda: self.get_entry_vals()) 
+        add_button = ctk.CTkButton(
+            self, text="Adicionar", command=lambda: self.get_entry_vals())
         add_button.grid(row=len(labels)+1, columnspan=2, pady=10,)
 
+    def get_entry_vals(self):  # função para pegar os valores da entry e jogar em um objeto
+        self.nome, self.unidade, self.quantidade, self.valor_uni, self.valor_total = [
+            entry.get() for entry in self.entries]
 
-    def get_entry_vals(self): #função para pegar os valores da entry e jogar em um objeto
-        self.nome, self.unidade, self.quantidade, self.valor_uni, self.valor_total  = [entry.get() for entry in self.entries]
-        
-   
-
-        if self.switch_var.get()=="on":
+        if self.switch_var.get() == "on":
             try:
                 self.valor_total = int(self.quantidade) * int(self.valor_uni)
-                
-                
-            except:
-                print('deu ruim')#adicionar messagebox BRUNO
-       
-            
 
-        
-        
-        new_item(self,Item,self.service_name) #criando um objeto com os valores obtido
+            except:
+                print('deu ruim')  # adicionar messagebox BRUNO
+
+        # criando um objeto com os valores obtido
+        new_item(self, Item, self.service_name)
         self.destroy()
-    
+
     def destroy(self):
         super().destroy()
 
     def switch_event(self):
         if self.switch_var.get() == "on":
             self.entries[-1].grid_remove()  # Remove a entrada de Valor Total
-            
-            
+
         else:
             self.entries[-1].grid()  # Reexibe a entrada de Valor Total
-            
+
 
 class LoadWindow(ctk.CTkToplevel):
     def __init__(self, *args, **kwargs):
@@ -121,17 +130,15 @@ class LoadWindow(ctk.CTkToplevel):
         self.label.pack(padx=20, pady=20)
 
         saves = obter_nomes_saves()
-        
 
         self.load_optmenu = ctk.CTkOptionMenu(self, values=saves)
         self.load_optmenu.pack()
 
-        self.get_load_btn= ctk.CTkButton(self,text='CARREGAR',command=self.get_load)
+        self.get_load_btn = ctk.CTkButton(
+            self, text='CARREGAR', command=self.get_load)
         self.get_load_btn.pack()
-
 
     def get_load(self):
         self.choosed_load = self.load_optmenu.get()
-        
+
         self.destroy()  # Destruir o TopLevel
-        
